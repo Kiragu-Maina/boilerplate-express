edffed8d537ebecccea86c272a030f211286296b
Kiragu Maina <jumaliko711@gmail.com>
2023-10-17 23:49:13 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,47 +1,19 @@
-console.log("Hello World");
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-absolutePath = __dirname + '/views/index.html'
-app.get('/', function(req, res){
- res.sendFile(absolutePath);
+const absolutePath = path.join(__dirname, 'views', 'index.html');
+const absolutePublic = path.join(__dirname, 'public');
+
+// Serve static files from the 'public' directory
+app.use(express.static(absolutePublic));
+
+// Define a route to serve the HTML file
+app.get('/', function(req, res) {
+  res.sendFile(absolutePath, function (err) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 });
-absolutepublic = __dirname + '/public'
-
-app.use(express.static(absolutepublic))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
