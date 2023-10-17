d31a953b4135b03bc146b2bc6fa6fbb5f68c14ef
Kiragu Maina <jumaliko711@gmail.com>
2023-10-17 23:15:34 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 console.log("Hello World");
 let express = require('express');
 let app = express();
+
 absolutePath = __dirname + '/views/index.html'
 app.get('/', function(req, res){
  res.sendFile(absolutePath);
 });
+absolutepublic = __dirname + '/public'
+
+app.use(express.static(absolutepublic))
 
 
 
