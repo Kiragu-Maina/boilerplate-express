6ab8494e04d3ec1ce48db546c8715da1eb8e271f
Kiragu Maina <jumaliko711@gmail.com>
2023-10-18 00:42:42 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,14 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const absolutePath = path.join(__dirname, 'views', 'index.html');
-const absolutePublic = path.join(__dirname, 'public');
-
-// Serve static files from the 'public' directory
-app.use(express.static(absolutePublic));
+// Serve asset files from the '/public' path
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Define a route to serve the HTML file
 app.get('/', function(req, res) {
+  const absolutePath = path.join(__dirname, 'views', 'index.html');
   res.sendFile(absolutePath, function (err) {
     if (err) {
       console.error(err);
