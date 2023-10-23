81d6fe43fcb9cdec4337a201f37301696d47c7fa
Kiragu Maina <jumaliko711@gmail.com>
2023-10-23 03:27:10 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -9,5 +9,8 @@ const absolutePath = __dirname + '/views/index.html';
 app.get('/', function(req, res) {
     res.sendFile(absolutePath)
 });
+app.get('/json', function(req,res) {
+    res.json({"message": "Hello json"})
+});
 
 module.exports = app;
