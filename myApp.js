b884af448668f6bbc847ba9a3421a5c5c6827270
Kiragu Maina <jumaliko711@gmail.com>
2023-10-23 03:36:13 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -11,7 +11,12 @@ app.get('/', function(req, res) {
     res.sendFile(absolutePath)
 });
 app.get('/json', function(req,res) {
-    res.json({"message": "Hello json"})
+    if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({"message": "HELLO JSON"})
+    }
+    else {
+        res.json({"message": "Hello json"})
+    }
 });
 
 module.exports = app;
